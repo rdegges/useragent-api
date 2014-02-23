2286b9e65c28bc6bf8ae7f814a250847fb23ce26
Randall Degges <r@rdegges.com>
2014-02-23 14:45:51 -0800

    Making 'fetch_uas' management command.
    This returns a list of 'good' user agents :)

--- manage.py
+++ manage.py
@@ -1,13 +1,55 @@
 """Management commands."""
 
 
+from BeautifulSoup import BeautifulSoup
 from flask.ext.script import Manager
+from requests import get
 
 from useragent import app
 
 
 ##### GLOBALS
 manager = Manager(app)
+allowed_agents = [
+    'america online browser',
+    'aolbuild',
+    'chrome',
+    'chromium',
+    'chromeplus',
+    'epiphany',
+    'firefox',
+    'iceweasel',
+    'konqueror',
+    'mozilla',
+    'namoroka',
+    'opera',
+    'safari',
+    'seamonkey',
+]
+
+
+##### SCRIPTS
+@manager.command
+def fetch_uas():
+    """
+    Scrape this page: http://www.useragentstring.com/pages/All/ (which returns
+    every User Agent string known to man), then strip the list down to only the
+    modern browser user agents.
+    """
+    resp = get('http://www.useragentstring.com/pages/All/')
+    if resp.status_code != 200:
+        print 'Failed to fetch: http://www.useragentstring.com/pages/All/'
+        return
+
+    soup = BeautifulSoup(resp.text)
+    user_agents = []
+    for user_agent in [elem.first().getText() for elem in soup.findAll('li')]:
+        for agent in allowed_agents:
+            if agent in user_agent.lower():
+                user_agents.append(user_agent)
+
+    for user_agent in user_agents:
+        print user_agent
 
 
 if __name__ == '__main__':
