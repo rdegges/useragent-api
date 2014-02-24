8d51cd35752132a506d7b359902300e1f202bfaa
Randall Degges <r@rdegges.com>
2014-02-23 17:49:30 -0800

    Making our API call dynamic.
    We now use PostgreSQL to randomly select our UserAgent.

--- useragent/api.py
+++ useragent/api.py
@@ -2,8 +2,10 @@
 
 
 from flask import jsonify
+from sqlalchemy import func
 
 from . import app
+from .models import UserAgent
 
 
 @app.route('/')
@@ -17,4 +19,4 @@ def get_random_user_agent():
           "ua": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.117 Safari/537.36",
         }
     """
-    return jsonify(ua='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.117 Safari/537.36')
+    return jsonify(ua=UserAgent.query.order_by(func.random()).first().string)
