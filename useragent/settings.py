ae9640028575c52d8426b1a9e55feae5a3810446
Randall Degges <r@rdegges.com>
2014-02-23 17:31:45 -0800

    Adding settings (for db).

--- useragent/settings.py
+++ useragent/settings.py
@@ -1,1 +1,13 @@
 """App configuration."""
+
+
+from os import environ
+
+
+##### DEBUG
+DEBUG = True if environ.get('DEBUG') else False
+
+
+##### DATABASE
+SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
+SQLALCHEMY_POOL_SIZE = 20
